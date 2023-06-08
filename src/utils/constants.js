14a99085cc263be095ed1336956a798aeb8fb204
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-06-09 00:02:04 +0530

    Made some changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const base_uri = "http://localhost:5000";
-export const base_uri = "https://social-media-backend-4n55.onrender.com";
+export const base_uri = "http://localhost:5000";
+// export const base_uri = "https://social-media-backend-4n55.onrender.com";
